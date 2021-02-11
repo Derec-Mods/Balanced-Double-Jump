93743468daf2bd5d751b7fb4bd839d300e9291ae
Derek Chen <53978637+DereC4@users.noreply.github.com>
2021-02-10 23:34:28 -0600

    Initial Commit

---
+++ src/main/java/net/derecsdoublejump/ServerProxyDerecsDoubleJumpMod.java
@@ -0,0 +1,24 @@
+package net.derecsdoublejump;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public class ServerProxyDerecsDoubleJumpMod implements IProxyDerecsDoubleJumpMod {
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
